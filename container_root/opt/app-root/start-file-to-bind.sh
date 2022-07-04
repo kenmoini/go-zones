cf8c20bf35d0485c59bf9f34a5366a99224a7aed
Ken Moini <ken@kenmoini.com>
2022-07-03 22:07:35 -0400

    bump for new container build process

--- container_root/opt/app-root/start-file-to-bind.sh
+++ container_root/opt/app-root/start-file-to-bind.sh
@@ -6,16 +6,18 @@ export NAMEDCONF=/opt/app-root/vendor/bind/named.conf
 export KRB5_KTNAME=/etc/named.keytab
 export DISABLE_ZONE_CHECKING=no
 export OPTIONS=""
+export SERVER_CONFIG_YAML="/etc/go-zones/server.yml"
+export GENERATED_DIR="/opt/app-root/generated-conf"
 
 echo -e "\nGENERATING ZONES AND CONFIG...\n"
-go-zones -mode file -source /etc/go-zones/zones.yml -dir=/opt/app-root/generated-conf
+go-zones -mode file -source "${SERVER_CONFIG_YAML}" -dir "${GENERATED_DIR}"
 
-echo -e "\nCOMBINING CONFIGS...\n"
-cat /opt/app-root/generated-conf/config/*.internal.forward.conf > /opt/app-root/generated-conf/config/internal-forward-zones.conf || true
-cat /opt/app-root/generated-conf/config/*.external.forward.conf > /opt/app-root/generated-conf/config/external-forward-zones.conf || true
-
-cat /opt/app-root/generated-conf/config/*.internal.reverse.conf > /opt/app-root/generated-conf/config/internal-reverse-zones.conf || true
-cat /opt/app-root/generated-conf/config/*.external.reverse.conf > /opt/app-root/generated-conf/config/external-reverse-zones.conf || true
+#echo -e "\nCOMBINING CONFIGS...\n"
+#cat /opt/app-root/generated-conf/config/*.internal.forward.conf > /opt/app-root/generated-conf/config/internal-forward-zones.conf || true
+#cat /opt/app-root/generated-conf/config/*.external.forward.conf > /opt/app-root/generated-conf/config/external-forward-zones.conf || true
+#
+#cat /opt/app-root/generated-conf/config/*.internal.reverse.conf > /opt/app-root/generated-conf/config/internal-reverse-zones.conf || true
+#cat /opt/app-root/generated-conf/config/*.external.reverse.conf > /opt/app-root/generated-conf/config/external-reverse-zones.conf || true
 
 echo -e "\nVALIDATING BIND DNS SERVER CONFIGURATION...\n"
 
--- container_root/opt/app-root/start-server.sh
+++ container_root/opt/app-root/start-server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export SERVER_CONFIG_YAML="/etc/go-zones/config.yml"
+export GENERATED_DIR="/opt/app-root/generated-conf"
+
 echo -e "\nSTARTING GO ZONES SERVER...\n"
-go-zones -mode server -config /etc/go-zones/config.yml
+go-zones -mode server -config "${SERVER_CONFIG_YAML}"
