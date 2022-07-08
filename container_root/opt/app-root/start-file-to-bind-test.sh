9dc998e889101a8d18f5811145791cb23ac67a33
Ken Moini <ken@kenmoini.com>
2022-07-08 19:05:44 -0400

    update tests

--- container_root/opt/app-root/start-file-to-bind-test.sh
+++ container_root/opt/app-root/start-file-to-bind-test.sh
@@ -42,12 +42,29 @@ echo -e "\nPERFORMING RECORD TEST...\n"
 
 DIG_TEST=$(dig @localhost idm.example.labs +short)
 echo "DIG TEST - idm.example.labs: ${DIG_TEST}"
+if [ "$DIG_TEST" != "10.12.0.10" ]; then
+  echo "DIG TEST FAILED"
+  exit 1
+fi
 
 DIG_TEST=$(dig @localhost priv.example.labs +short)
 echo "DIG TEST - priv.example.labs: ${DIG_TEST}"
+if [ "$DIG_TEST" != "192.168.0.11" ]; then
+  echo "DIG TEST FAILED"
+  exit 1
+fi
 
 DIG_TEST=$(dig @localhost -x 10.12.0.10 +short)
 echo "DIG TEST - -x 10.12.0.10: ${DIG_TEST}"
+if [ "$DIG_TEST" != "idm.example.labs." ]; then
+  echo "DIG TEST FAILED"
+  exit 1
+fi
 
 DIG_TEST=$(dig @localhost -x 192.168.0.11 +short)
 echo "DIG TEST - -x 192.168.0.11: ${DIG_TEST}"
+echo "DIG TEST - -x 10.12.0.10: ${DIG_TEST}"
+if [ "$DIG_TEST" != "priv.example.labs." ]; then
+  echo "DIG TEST FAILED"
+  exit 1
+fi
